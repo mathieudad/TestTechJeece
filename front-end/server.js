d44debb5c32f60d8d635ea07e3c7a151cf1b506a
clemoctohub <clemfages@gmail.com>
2020-11-09 22:51:57 +0100

    Code Finished - test to do

--- front-end/server.js
+++ front-end/server.js
@@ -21,7 +21,8 @@ app.set('view engine', 'pug')
 
 ///////////////module/////////////////////////
 
-var sess;
+var sess,mail1,mail2;
+var listPeople = [];
 
 //////////////////////////////GET////////////////////
 app.get('/create', (req, res) => {
@@ -31,14 +32,14 @@ app.get('/create', (req, res) => {
 
 app.get('/profil', (req, res) => {
   res.set('Content-Type', 'text/html');
-  res.render('index',{lastName : sess.lastName, firstName : sess.firstName, team : sess.team.number, dept : sess.team.dept})
+  res.render('index',{lastName : sess.lastName, firstName : sess.firstName, team : sess.team.number, dept : sess.team.dept, eMail : sess.eMail})
 })
 
 app.get('/home_page', (req, res) => {
   res.set('Content-Type', 'text/html');
-  res.sendfile("home_page.html")
-  const url = "http://localhost:3000/home_page";
-  fetch(url, {
+  console.log(req.url)
+  const url = "http://localhost:3000";
+  fetch(url+req.url, {
       method: "GET",
       headers: { 'Content-Type': 'application/json' },
       //application/x-www-form-urlencoded
@@ -46,7 +47,8 @@ app.get('/home_page', (req, res) => {
   })
   .then(res => res.json())
   .then( data => {
-      console.log(data[0])
+    console.log(data)
+    res.render('home_page',{data : data, mail : sess.eMail})
   })
   .catch(err => console.log(err));
 })
@@ -58,7 +60,57 @@ app.get('/login', (req, res) => {
 
 app.get('/chat', (req, res) => {
   res.set('Content-Type', 'text/html');
-  res.sendfile('chat.html')
+
+  var temp = req.url,temp1,temp2;
+  const chars = temp.split('');
+  var condi = true;
+  var j,k;
+  for(var i=12;i<temp.length && condi===true;i++){
+    if(temp[i]=='%'){
+      condi = false
+      j = i
+    }
+  }
+  temp1 = temp.substring(12,j)
+  condi = true;
+  for(var i=13;i<temp.length && condi===true;i++){
+    if(temp[i]=='='){
+      condi = false
+      k = i
+    }
+  }
+  condi=true;
+  for(var i=k;i<temp.length && condi===true;i++){
+    if(temp[i]=='%'){
+      condi = false;
+      j = i;
+    }
+  }
+  temp2 = temp.substring(k+1,j);
+  if(temp1!="chat" && temp1!=""){
+    console.log("c diff1")
+    mail1 = temp1+"@societe.com";
+  }
+  if(temp2!="/chat"){
+    console.log("c diff1")
+    mail2 = temp2+"@societe.com";
+  }
+
+  console.log("mail1 = "+mail1+" mail2 = "+mail2)
+
+  const url = "http://localhost:3000";
+  console.log(url+"/chat/"+mail1+"/"+mail2)
+  fetch(url+"/chat/"+mail1+"/"+mail2, {
+      method: "GET",
+      headers: { 'Content-Type': 'application/json' },
+      //application/x-www-form-urlencoded
+  })
+  .then(res => res.json())
+  .then( data => {
+    console.log(data)
+    res.render('chat',{mail1 : mail1, mail2 : mail2, data : data})
+  })
+  .catch(err => console.log(err));
 })
 
 /////////////////////////////////POST////////////////////
@@ -90,6 +142,7 @@ app.post('/create',(req, res) => {
   .then(res => res.json())
   .then(json => {
     if(json){
+      sess = json;
       console.log(json);
       res.redirect('/home_page')
     }
@@ -109,7 +162,7 @@ app.post('/login',(req, res) => {
   })
   .then(res => res.json())
   .then(json => {
-    if(json){
+    if(json.eMail){
       sess = json;
       console.log(json)
       res.redirect('/home_page');
@@ -117,6 +170,24 @@ app.post('/login',(req, res) => {
     else{
       console.log(json);
       res.sendfile('login.html')
+    }
+  });
+})
+
+app.post('/chat',(req, res) => {
+  fetch("http://localhost:3000/chat", {
+    method: "post",
+    body: JSON.stringify(req.body),
+    headers: { 'Content-Type': 'application/json' },
+  })
+  .then(res => res.json())
+  .then(json => {
+    if(json){
+      console.log(json);
+      res.redirect('/chat')
+    }
+    else{
+      console.log(json);
     }
   });
 })
